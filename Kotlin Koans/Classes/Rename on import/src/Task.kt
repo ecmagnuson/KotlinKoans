adff40fc578324457e3f11f6889d77c2df4bbb85
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-05-13 01:42:35 -0500

    finished Comparison

--- Kotlin Koans/Classes/Rename on import/src/Task.kt
+++ Kotlin Koans/Classes/Rename on import/src/Task.kt
@@ -1,10 +1,23 @@
-// import kotlin.random.Random
-// import java.util.Random
+import kotlin.random.Random as KRandom
+import java.util.Random as JRandom
 
 fun useDifferentRandomClasses(): String {
     return "Kotlin random: " +
-            // KRandom.nextInt(2) +
+            KRandom.nextInt(2) +
             " Java random:" +
-            // JRandom().nextInt(2) +
+            JRandom().nextInt(2) +
             "."
+}
+
+fun useDiffRandClasses(): String { //triple strings :D
+    return """
+        Kotlin random: 
+        ${KRandom.nextInt(2)}
+        Java random:
+        ${JRandom().nextInt(2)}
+    """.trimIndent()
+}
+
+fun main() {
+    println(useDiffRandClasses())
 }
--- Kotlin Koans/Classes/Smart casts/src/Task.kt
+++ Kotlin Koans/Classes/Smart casts/src/Task.kt
@@ -1,10 +1,33 @@
 fun eval(expr: Expr): Int =
         when (expr) {
-            is Num -> TODO()
-            is Sum -> TODO()
+            is Num -> expr.value
+            is Sum -> eval(expr.left) + eval(expr.right)
             else -> throw IllegalArgumentException("Unknown expression")
         }
 
 interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
+class Num(val value: Int) : Expr  {
+
+    //2ndary constructor must call primary constructor.
+    //i have the secondary one that accepts no params and defaults value to 0
+    constructor(): this(0)
+
+}
+class Sum(val left: Expr, val right: Expr) : Expr //really weird to see a class on just one line
+// but the fact that the parameters have 'val' (var would also work) means that they are
+//properties of the class and have implicit getters and setters created. no need to make our own.. weird
+
+fun main() {
+
+    val variable1 = Num() //num is 0
+    println(eval(variable1)) //returns 0 because is Num
+
+    //Num class is of type Expr because it inherits from it?
+    val variable2 = Sum(Num(10), Num(20))
+    println(eval(variable2))
+
+
+}
+
+
+
